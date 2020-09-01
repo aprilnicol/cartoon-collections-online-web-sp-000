77401c4d3c44c59bbd6a31605fe4a1c2bd52bffa
April Adams <illusionlace@gmail.com>
2020-09-01 13:25:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
+names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+names.each_with_index do |name, index|
+  puts "#{index + 1}. #{name}"
+end 
+
 end
 
 def summon_captain_planet# code an argument here
