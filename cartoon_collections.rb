ca6f99891d77bec66b51c885d11f346d28115460
April Adams <illusionlace@gmail.com>
2020-09-01 13:32:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
   calls.any? {|i| i.length > 4}
-  
+
 end
 def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
